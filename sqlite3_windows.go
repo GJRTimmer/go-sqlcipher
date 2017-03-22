6a81afa9d690b1622f641127fd89ac290e740f1c
Gert-Jan Timmer <gjr.timmer@gmail.com>
2017-03-22 22:50:52 +0100

    Add LDFLAG
    Add CGO LDFLAG -lgdi32 for succesfull compilation on Win64

--- sqlite3_windows.go
+++ sqlite3_windows.go
@@ -9,6 +9,6 @@ package sqlite3
 /*
 #cgo CFLAGS: -I. -fno-stack-check -fno-stack-protector -mno-stack-arg-probe
 #cgo windows,386 CFLAGS: -D_USE_32BIT_TIME_T
-#cgo LDFLAGS: -lmingwex -lmingw32
+#cgo LDFLAGS: -lmingwex -lmingw32 -lgdi32
 */
 import "C"
